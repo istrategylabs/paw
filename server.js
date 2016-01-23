8aec5a11a1842c1707c7c94c7b9c611fcd5e9895
Andrew Krawchyk <andrew@isl.co>
2016-01-22 19:00:28 -0500

    setup data from mock islapi to redis to frontend

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 var express = require('express');
 var bodyParser = require('body-parser');
 var redis = require('redis');
+var request = require('request');
 
 var redisURL = process.env.REDIS_URL;
 var client = redis.createClient(redisURL);
@@ -10,6 +11,7 @@ var app = express();
 app.set('port', (process.env.PORT || 3000));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
 
 var forceSSL = function (req, res, next) {
   if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -21,6 +23,32 @@ var forceSSL = function (req, res, next) {
 if (env === 'production') {
   app.use(forceSSL);
 }
+
+
+// load data into redis from isl api
+var token = process.env.ISL_API_TOKEN;
+request.get({
+  url: 'https://isl-api-staging.herokuapp.com/api/v2/pets/',
+  headers: {
+    'Authorization': 'Token ' + token
+  }
+}, function(error, response, body) {
+  client.del('dogs');
+  var dogs;
+
+  if (!error && response.statusCode == 200) {
+    dogs = JSON.parse(body);
+  } else {
+    // read dogs list from fixture
+    dogs = require('./fixtures/dogs.json');
+  }
+
+  // set dogs to redis
+  dogs.forEach(function(dog) {
+    client.lpush('dogs', JSON.stringify(dog));
+  });
+});
+
 
 app.get('/api/dog/:id', function(req, res) {
   res.json({ id: req.params.id });
@@ -35,13 +63,20 @@ app.post('/api/dog', function(req, res) {
 });
 
 app.get('/api/dogs', function(req, res) {
-  // client.keys('*', function(err, keys) {
-  //   res.json({ dogs: keys });
-  // });
-  res.json([
-    { id: 1, name: 'watson' },
-    { id: 2, name: 'maggie' }
-  ]);
+  client.llen('dogs', function(err, len) {
+    console.log(len);
+    client.lrange('dogs', 0, len-1, function(err, dogs) {
+      console.log(dogs);
+      res.json(dogs.map(function(dog) {
+        return JSON.parse(dog);
+      }));
+    });
+  });
+
+  // res.json([
+  //   { id: 1, name: 'watson' },
+  //   { id: 2, name: 'maggie' }
+  // ]);
 });
 
 app.post('/api/event', function(req, res) {
