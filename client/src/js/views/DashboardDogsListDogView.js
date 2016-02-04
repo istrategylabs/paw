ce238443585492a88c857d71e9d113bc1b9184de
Alex Martinez <amart1892@gmail.com>
2016-02-04 14:37:53 -0500

    pulled updates from master into dash-sidebar

--- client/src/js/views/DashboardDogsListDogView.js
+++ client/src/js/views/DashboardDogsListDogView.js
@@ -15,7 +15,7 @@ var DashboardDogsListDogView = Backbone.View.extend({
   initialize: function() {
     this.render();
   },
-
+ 
   render: function() {
     this.$el.html(this.template(this.model.attributes));
     return this;
