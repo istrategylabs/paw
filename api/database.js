ac5853f7d6c90f4dc4208497609b48984b3d4f38
Andrew Krawchyk <akrawchyk@gmail.com>
2016-03-16 16:18:13 -0400

    separate redis population

--- api/database.js
+++ api/database.js
@@ -1,3 +1,4 @@
+var TOKEN = process.env.ISL_API_TOKEN;
 var redis = require('redis');
 var redisURL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 var client = redis.createClient(redisURL);
@@ -14,50 +15,51 @@ client.on('error', function(err) {
 });
 
 
-// initialize with data from isl api or fixture
-var TOKEN = process.env.ISL_API_TOKEN;
-request.get({
-  url: 'https://api.isl.co/v1/pets/',
-  headers: {
-    'Authorization': 'Token ' + TOKEN
+function loadDogFromAPI(dog) {
+  if (dog.display_id && dog.pet_link) {
+    // TODO move detail view to list view in api and remove this loop
+    // haven't requested dog detail view yet
+    request.get({
+      url: dog.pet_link,
+      headers: {
+        'Authorization': 'Token ' + TOKEN
+      }
+    }, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        dog = JSON.parse(body);
+        Dogs.save(dog);
+      } else {
+        // read dog from fixture
+        var d = require('./fixtures/dog.json');
+        dog = find(d, { display_id: dog.display_id});
+        Dogs.save(dog);
+      }
+    });
   }
-}, function(error, response, body) {
-  var dogs;
+}
 
-  if (!error && response.statusCode == 200) {
-    dogs = JSON.parse(body);
-  } else {
-    // read dogs list from fixture
-    dogs = require('./fixtures/dogs.json');
-  }
 
-  // set dogs to redis set
-  // set dog to redis key
-  // set btle_device to redis set
-  // set dog:id to btle_device
-  dogs.forEach(function(dog) {
-    if (dog.display_id && dog.pet_link) {
-      // TODO move detail view to list view in api and remove this loop
-      // haven't requested dog detail view yet
-      request.get({
-        url: dog.pet_link,
-        headers: {
-          'Authorization': 'Token ' + TOKEN
-        }
-      }, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          dog = JSON.parse(body);
-          Dogs.save(dog);
-        } else {
-          // read dog from fixture
-          var d = require('./fixtures/dog.json');
-          dog = find(d, { display_id: dog.display_id});
-          Dogs.save(dog);
-        }
-      });
+function populateDB() {
+  // initialize with data from isl api or fixture
+  request.get({
+    url: 'https://api.isl.co/v1/pets/',
+    headers: {
+      'Authorization': 'Token ' + TOKEN
     }
-  });
-});
+  }, function(error, response, body) {
+    var dogs;
 
+    if (!error && response.statusCode == 200) {
+      dogs = JSON.parse(body);
+    } else {
+      // read dogs list from fixture
+      dogs = require('./fixtures/dogs.json');
+    }
+
+    dogs.forEach(loadDogFromAPI);
+  });
+}
+
+populateDB();
 
 module.exports = client;
